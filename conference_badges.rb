90dea01824b3184513b8e0699e83fd4564c5103a
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-12 22:29:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,16 +1,16 @@
-def badge_maker(name) do
+def badge_maker(name) 
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(names) do
+def batch_badge_creator(names) 
   badge_messages = names.collect {|name| badge_maker(name)}
   badge_messages
 end
 
-#def assign_rooms(names) do
+#def assign_rooms(names) 
   
 #end
 
-#def printer do
+#def printer 
   
 #end
