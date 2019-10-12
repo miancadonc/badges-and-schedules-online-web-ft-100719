be1978df39af4dcc2128d675c60de32ccb8ba47e
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-12 22:40:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 def assign_rooms(names)
   assignment_messages = []
   names.each_with_index do |name, i|
-    assignment_messages << "Hello, #{name}! You'll be assigned to room #{i}!"
+    assignment_messages << "Hello, #{name}! You'll be assigned to room #{i+1}!"
   end
   assignment_messages
 end
